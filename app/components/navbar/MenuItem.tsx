01e0b89ecca83a3e08c26ec7c1162206000b6601
Febriana Kusumah <kusumahfebriana@gmail.com>
2024-03-16 17:46:34 +0700

    register modal

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -11,14 +11,8 @@ const MenuItem: React.FC<MenuItemProps> = ({
 }) => {
   return (
     <div
-      onClick={() => {}}
-      className="
-        px-4
-        py-3
-        hover:bg-neutral-100
-        transition
-        font-semiold
-      "
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semiold"
     >
       {label}
     </div>
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,21 +7,11 @@ const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div
-        className="
-          py-4
-          border-b-[1px]
-        "
+        className="py-4 border-b-[1px]"
       >
         <Container>
           <div
-            className="
-              flex
-              flex-row
-              items-center
-              justify-between
-              gap-3
-              md:gap-0
-            "
+            className="flex flex-row items-center justify-between gap-3 md:gap-0"
           >
             <Logo />
             <Search />
