debb22c4decaaf6ce9b024cdd937bddb701b5aa4
Febriana Kusumah <febriana.kusumah@nexsoft.co.id>
2024-03-19 16:37:23 +0700

    fix yaml

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true
-  }
+  },
+  reactStrictMode: true,
+  webpack: (config, { nextRuntime }) => {
+    if (typeof nextRuntime === "undefined") {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
